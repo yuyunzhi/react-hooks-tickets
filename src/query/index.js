24bc5aa35f033a073ea282abc31129f6f5883751
yuyunzhi <lambert1129@foxmail.com>
2020-06-03 11:16:05 +0800

    feat:配置webpack

--- src/query/index.js
+++ src/query/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './index/App.jsx';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../serviceWorker';
 import 'normalize.css/normalize.css'
 
 ReactDOM.render(
