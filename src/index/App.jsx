f087ae95bbb8f7b581bff7f346aee0d80ec6861c
yuyunzhi <lambert1129@foxmail.com>
2020-06-05 11:44:22 +0800

    feat:update

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -5,9 +5,9 @@ import './App.css';
 
 function App(props) {
   return (
-    <div className="App">
+      <div className="App">
 
-    </div>
+      </div>
   );
 }
 
--- src/order/index.js
+++ src/order/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import store from './store';
-import * as serviceWorker from '../serviceWorker';
-import 'normalize.css/normalize.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store";
+import * as serviceWorker from "../serviceWorker";
+import "normalize.css/normalize.css";
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
